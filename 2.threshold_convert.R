69c8832bc837846c80dd92548d9a363a28cc19a8
Michael Andric <andric.michael@gmail.com>
2013-09-10 16:45:28 +0200

    update threshfilename

--- 2.threshold_convert.R
+++ 2.threshold_convert.R
@@ -3,13 +3,14 @@ library(bct)
 Args <- Sys.getenv("R_ARGS")
 g <- noquote(strsplit(Args," ")[[1]][1])
 h <- noquote(strsplit(Args," ")[[1]][2])
+thresh <- .5
 
 setwd("/mnt/tier2/urihas/Andric/pandit")
 
 corrmat <- paste(g,"_",h,"_formatted.txt.bin.corr", sep = "")
-threshmat <- paste(g,"_",h,"_formatted.txt.bin.corr.thresh", sep = "")
+threshmat <- paste(g,"_",h,"_formatted.txt.bin.corr.thresh",thresh, sep = "")
 blondelmat <- paste(g,"_",h,"_formatted.txt.bin.corr.srcdst", sep = "")
 print(paste("Thresholding matrix for",g,h))
-fthreshold_absolute(corrmat,threshmat, .5, 0)
+fthreshold_absolute(corrmat,threshmat, thresh, 0)
 print(paste("Converting to blondel format"))
 blondel_convert(threshmat, blondelmat)
