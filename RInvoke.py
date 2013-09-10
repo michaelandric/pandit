0ca23f17d48449142e982eaf396210b9fcdc9eac
Michael Andric <andric.michael@gmail.com>
2013-09-10 15:43:39 +0200

    join RInvoke args as strings

--- RInvoke.py
+++ RInvoke.py
@@ -5,6 +5,6 @@ import sys
 from subprocess import call
 
 
-os.environ["R_ARGS"] = sys.argv[1:]
+os.environ["R_ARGS"] = ' '.join(sys.argv[1:])
 
 call("R CMD BATCH --vanilla "+sys.argv[0], shell = True)
