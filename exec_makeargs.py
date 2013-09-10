5de4d1fe7cbc73bde21d62aea0ac42b59613ef47
Michael Andric <andric.michael@gmail.com>
2013-09-10 15:00:10 +0200

    adding python code to set up arguments in scripts

---
+++ exec_makeargs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+Here's how you use this on the command line:
+python exec_makesubmitargs.py >> submit.try
+where 'submit.try' is the condor_submit file that you're adding arguments to
+"""
+
+from makesubmitargs import makeargs as mm
+
+groups = ["ctrl", "pandit"]
+hemispheres = ["lh", "rh"]
+
+for g in groups:
+    for h in hemispheres:
+        mm.fcorr_args(g, h)
---
+++ makeargs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+from optparse import OptionParser
+
+class MakeArgs:
+
+    def get_opts(self):
+        desc = """Generate args for condor submit scripts"""
+        self.usage = "usage: %prog [options]"
+        self.parser = OptionParser(description = desc, version = "%prog September 2013")
+        self.parser.add_option("--subject", desc = "subject",
+                                help = "specify the subject")
+        self.parser.add_option("--arg1", desc = "arg1",
+                                help = "give the first argument")
+                                
+        (self.options, args) = self.parser.parse_args()
+        
+    def fcorr_args(self, group, arg1):
+        """
+        'group' is either ctrl or pandit
+        'arg1' is hemisphere
+        """
+        print "arguments    = 1.corr_matrix.R "+group+" "+arg1+" \nqueue \n"
+
+makearg = MakeArgs()
