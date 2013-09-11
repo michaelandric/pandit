6794a602fa6a4a3740e3a98e3c31c54cf415051f
michaelandric <andric.michael@gmail.com>
2013-09-11 14:28:13 +0200

    this is for combo lh and rh

---
+++ 1A.corr_matrix.R
@@ -0,0 +1,9 @@
+## Get data and run fcorr
+library(bct)
+Args <- Sys.getenv("R_ARGS")
+g <- noquote(strsplit(Args," ")[[1]][1])
+h <- noquote(strsplit(Args," ")[[1]][2])
+
+setwd("/mnt/tier2/urihas/Andric/pandit")
+tmp_name <- paste(g,"_lhrh_formatted.txt", sep = "") ## data have already been hand formatted with 34 regions (rows) by 21 participants (cols)
+fcorr(tmp_name, 68, 21)
