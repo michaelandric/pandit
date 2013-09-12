9d585b5754ebae01a4dcc675e19baa464da2ff55
michaelandric <andric.michael@gmail.com>
2013-09-12 14:58:34 +0200

    update with margins

--- 5.moduleplot.R
+++ 5.moduleplot.R
@@ -14,10 +14,16 @@ for (n in nome)
     nomi <- c(nomi, paste(noquote(strsplit(n,"_"))[[1]][2]))
 }
 
-pdf("pandit_moduleplot.pdf")
-heatmap(matrix(p[,2]+1,ncol=2), Rowv = NA, Colv = NA, scale = "none", labRow = nomi, col = thepal) ## gives upside-down (34 at the top and the 1's at the bottom)
+df.p <- data.frame(matrix(p[,2]+1,ncol=2))
+colnames(df.p) <- c("lh", "rh")
+
+pdf("pandit_moduleplot.pdf", width = 6)
+heatmap(data.matrix(df.p), Rowv = NA, Colv = NA, scale = "none", labRow = nomi, col = thepal, margins = c(5, 10), main = "Pandit") ## gives upside-down (34 at the top and the 1's at the bottom)
 dev.off()
 
-pdf("ctrl_moduleplot.pdf")
-heatmap(matrix(c[,2]+1,ncol=2), Rowv = NA, Colv = NA, scale = "none", labRow = nomi, col = thepal)
+df.c <- data.frame(matrix(c[,2]+1,ncol=2))
+colnames(df.c) <- c("lh", "rh")
+
+pdf("ctrl_moduleplot.pdf", width = 6)
+heatmap(data.matrix(df.c), Rowv = NA, Colv = NA, scale = "none", labRow = nomi, col = thepal, margins = c(5, 10), main = "Ctrl")
 dev.off()
