af2cb99ea44924a4a32a96dc4c7d9974abcd1494
Michael Andric <andric.michael@gmail.com>
2015-05-14 18:38:58 +0200

    set up do_graph_evals for random

--- do_graph_evals.py
+++ do_graph_evals.py
@@ -10,6 +10,7 @@ import time
 import pandas as pd
 import numpy as np
 import graph_evals as ge
+import networkx as nx
 
 
 if __name__ == '__main__':
@@ -17,6 +18,13 @@ if __name__ == '__main__':
     os.chdir(os.environ['pnd'])
     print os.getcwd()
 
+    dat_dir = os.environ['pnd']
+    random_dir = dat_dir+'/random/'
+    if not os.path.exists(random_dir):
+        os.makedirs(random_dir)
+    random_graph_dir = random_dir+'/graphs/'
+    if not os.path.exists(random_graph_dir):
+        os.makedirs(random_graph_dir)
     df = pd.read_csv('pandit_data.csv')
     p_df = df.iloc[0:21, 1:]
     c_df = df.iloc[21:42, 1:]
@@ -25,15 +33,19 @@ if __name__ == '__main__':
     dat.append(p_df)
     dat.append(c_df)
     pc_dat = dict(zip(subj_list, dat))
+    n_nodes = 148
+    niter = 100
+    mod_loc = 'modularity'
+    mod_dir = os.path.join(random_dir, mod_loc)
+    if not os.path.exists(mod_dir):
+        os.makedirs(mod_dir)
 
     for d in pc_dat:
-        for thresh_dens in np.arange(.2, .51, .1):
+        for thresh_dens in np.arange(.1, .51, .1):
             print 'Thresh: %s' % thresh_dens
             print time.ctime()
             subjid = d
-            dat_dir = os.environ['pnd']
-            graph_loc = 'graphs'
-            graph_dir = os.path.join(dat_dir, graph_loc)
+            graph_dir = os.path.join(dat_dir, 'graphs')
             if not os.path.exists(graph_dir):
                 os.makedirs(graph_dir)
             graph_outname = '%s.dens_%s.edgelist.gz' % (subjid, thresh_dens)
@@ -42,30 +54,26 @@ if __name__ == '__main__':
                            thresh_dens, graph_dir,
                            os.path.join(graph_dir, graph_outname))
 
-            # making graph:
-            avg_r = np.zeros(1)
-            avg_r[0] = gr.make_graph()
-            avg_r_val_name = 'Avg_r_val_%s.dens_%s.txt' % (subjid, thresh_dens)
-            avg_r_out = os.path.join(graph_dir, avg_r_val_name)
-            np.savetxt(avg_r_out, avg_r, fmt='%.4f')
-
-            # get modularity and trees
-            print 'Doing modularity evaluation... '
-            print time.ctime()
-            n_nodes = 148
             g = gr.make_networkx_graph(n_nodes)
+            for n in xrange(niter):
+                deg = int(np.mean(g.degree().values()))
+                g_rand = nx.random_regular_graph(deg, n_nodes)
+                outg_pref = 'iter%d_rand_%s.dens_%s.edgelist.gz' % \
+                    (n, subjid, thresh_dens)
+                outg = os.path.join(random_graph_dir, outg_pref)
+                nx.write_edgelist(g_rand, outg, data=False)
 
-            mod_loc = 'modularity'
-            mod_dir = os.path.join(dat_dir, mod_loc)
-            if not os.path.exists(mod_dir):
-                os.makedirs(mod_dir)
-            niter = 100
-            Qs = np.zeros(niter)
-            trees = np.zeros(n_nodes*niter).reshape(n_nodes, niter)
-            for i in xrange(niter):
-                trees[:, i], Qs[i] = gr.get_modularity(g)
-            Qs_outname = '%s.dens_%s.Qval' % (subjid, thresh_dens)
-            np.savetxt(os.path.join(mod_dir, Qs_outname), Qs, fmt='%.4f')
-            trees_outname = '%s.dens_%s.trees' % (subjid, thresh_dens)
-            np.savetxt(os.path.join(mod_dir, trees_outname),
-                       trees, fmt='%i')
+                # get modularity and trees
+                print 'Doing modularity evaluation... '
+                print time.ctime()
+                Qs = np.zeros(niter)
+                trees = np.zeros(n_nodes*niter).reshape(n_nodes, niter)
+                for i in xrange(niter):
+                    trees[:, i], Qs[i] = gr.get_modularity(g_rand)
+                Qs_outname = 'iter%d_rand_%s.dens_%s.Qval' % \
+                    (n, subjid, thresh_dens)
+                np.savetxt(os.path.join(mod_dir, Qs_outname), Qs, fmt='%.4f')
+                trees_outname = 'iter%d_rand_%s.dens_%s.trees' % \
+                    (n, subjid, thresh_dens)
+                np.savetxt(os.path.join(mod_dir, trees_outname),
+                           trees, fmt='%i')
