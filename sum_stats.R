c025cdd61b7dfe527aed6205fb457b3a0a0939b0
Michael Andric <andric.michael@gmail.com>
2015-05-18 12:11:21 +0200

    initial look at summary modularity stats with sum_stats.R

---
+++ sum_stats.R
@@ -0,0 +1,20 @@
+# plot node roles 
+library(dplyr)
+library(ggplot2)
+pnd_dir <- paste('~/Documents/workspace/pandit/')
+setwd(pnd_dir)
+print(getwd())
+group_names <- c('pandit', 'ctrl')
+thresh_dens <- c('0.1')
+
+for (g in group_names)
+{
+    f_name <- paste(g, 'dens', thresh_dens, sep='_')
+    assign(paste(f_name), as.matrix(read.table(paste('modularity/', g, '.dens_', thresh_dens, '.Qval', sep=''))))
+    rand_name <- paste('rand', g, 'dens', thresh_dens, sep='_')
+    assign(paste(rand_name), as.matrix(read.table(paste('random/rand_', g, '.dens_', thresh_dens, '.Qval', sep=''))))
+}
+print(ks.test(pandit_dens_0.1, ctrl_dens_0.1))
+print(summary(pandit_dens_0.1))
+print(summary(ctrl_dens_0.1))
+
